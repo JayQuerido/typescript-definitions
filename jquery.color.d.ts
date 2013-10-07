75204b017fa91ea5218324e06ec870c290e56e44
slagr <itareo@yandex.ru>
2013-10-07 23:14:13 +0400

    added jquery.color

---
+++ jquery.color.d.ts
@@ -0,0 +1,28 @@
+// incomplete definitions for jquery.color:
+// https://github.com/jquery/jquery-color
+
+declare enum RGBA {red, green, blue, alpha}
+
+interface JqueryColor {
+    red(): number;
+    red(val:number): JqueryColor;
+    green(): number;
+    green(val:number): JqueryColor;
+    blue(): number;
+    blue(val:number): JqueryColor;
+    hue(): number;
+    hue(val:number): JqueryColor;
+    saturation(): number;
+    saturation(val:number): JqueryColor;
+    lightness(): number;
+    lightness(val:number): JqueryColor;
+
+    rgba(): RGBA
+    rgba(red:number, green:number, blue:number, alpha:number): void;
+    rgba(rgba:{red:number; green:number; blue:number; alpha:number}): void;
+    rgba(RGBA): void;
+
+    toRgbaString():string;
+    toHslaString():string;
+    toHexString(includeAlpha:boolean):string;
+}
